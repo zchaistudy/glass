7004bb8de93b11240aa34f8dd2328ba865e03113
zh <yzhiyzhi@126.com>
2018-06-02 21:53:38 +0800

    添加超声波模块

---
+++ codes/User/UltrasonicWave/bsp_GeneralTim.c
@@ -0,0 +1,127 @@
+
+#include "bsp_GeneralTim.h"
+
+// 定时器输入捕获用户自定义变量结构体定义
+TIM_ICUserValueTypeDef TIM_ICUserValueStructure = {0,0,0,0};
+// 中断优先级配置
+
+
+static void TIM3_NVIC_Config(void)
+{
+    NVIC_InitTypeDef NVIC_InitStructure; 	
+		// 设置中断来源
+    NVIC_InitStructure.NVIC_IRQChannel = TIM3_IRQn ;	
+		// 设置主优先级为 0
+    NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;	 
+	  // 设置抢占优先级为3
+    NVIC_InitStructure.NVIC_IRQChannelSubPriority = 2;	
+    NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+    NVIC_Init(&NVIC_InitStructure);
+}
+
+
+static void TIM3_GPIO_Config(void) 
+{
+  GPIO_InitTypeDef GPIO_InitStructure;
+
+  // 输入捕获通道 GPIO 初始化
+  RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING;
+	
+  GPIO_InitStructure.GPIO_Pin =  GPIO_Pin_6;
+  GPIO_Init(GPIOA, &GPIO_InitStructure);	
+	
+  GPIO_InitStructure.GPIO_Pin =  GPIO_Pin_7;
+  GPIO_Init(GPIOA, &GPIO_InitStructure);
+}
+
+static void TIM3_Mode_Config(void)
+{
+	TIM_TimeBaseInitTypeDef  TIM_TimeBaseStructure;
+	TIM_ICInitTypeDef TIM_ICInitStructure;
+	
+  // 开启定时器时钟,即内部时钟CK_INT=72M
+	GENERAL_TIM_APBxClock_FUN(RCC_APB1Periph_TIM3,ENABLE);
+
+/*--------------------时基结构体初始化-------------------------*/	
+  
+	// 自动重装载寄存器的值，累计TIM_Period+1个频率后产生一个更新或者中断
+	TIM_TimeBaseStructure.TIM_Period=GENERAL_TIM_PERIOD;	
+	// 驱动CNT计数器的时钟 = Fck_int/(psc+1)
+	TIM_TimeBaseStructure.TIM_Prescaler= GENERAL_TIM_PSC;	
+	// 时钟分频因子 ，配置死区时间时需要用到
+	TIM_TimeBaseStructure.TIM_ClockDivision=TIM_CKD_DIV1;		
+	// 计数器计数模式，设置为向上计数
+	TIM_TimeBaseStructure.TIM_CounterMode=TIM_CounterMode_Up;		
+	// 重复计数器的值，没用到不用管
+	TIM_TimeBaseStructure.TIM_RepetitionCounter=0;	
+	// 初始化定时器
+	TIM_TimeBaseInit(TIM3, &TIM_TimeBaseStructure);
+
+	/*--------------------输入捕获结构体初始化-------------------*/	
+	// 输入捕获信号的极性配置
+	TIM_ICInitStructure.TIM_ICPolarity = GENERAL_TIM_STRAT_ICPolarity;
+	// 输入通道和捕获通道的映射关系，有直连和非直连两种
+	TIM_ICInitStructure.TIM_ICSelection = TIM_ICSelection_DirectTI;
+	// 输入的需要被捕获的信号的分频系数
+	TIM_ICInitStructure.TIM_ICPrescaler = TIM_ICPSC_DIV1;
+	// 输入的需要被捕获的信号的滤波系数
+	TIM_ICInitStructure.TIM_ICFilter = 0;
+		
+	TIM_ICInitStructure.TIM_Channel = TIM_Channel_1;
+	// 定时器输入捕获初始化
+	TIM_ICInit(TIM3, &TIM_ICInitStructure);	
+		// 配置输入捕获的通道，需要根据具体的GPIO来配置
+	TIM_ICInitStructure.TIM_Channel = TIM_Channel_2;
+	// 定时器输入捕获初始化
+	TIM_ICInit(TIM3, &TIM_ICInitStructure);
+
+	// 清除更新和捕获中断标志位
+	TIM_ClearFlag(TIM3, TIM_FLAG_Update|TIM_IT_CC1);
+    TIM_ClearFlag(TIM3, TIM_FLAG_Update|TIM_IT_CC2);
+
+  // 开启更新和捕获中断  
+	TIM_ITConfig (TIM3, TIM_IT_Update | TIM_IT_CC2, ENABLE );
+	TIM_ITConfig (TIM3, TIM_IT_Update | TIM_IT_CC1, ENABLE );
+	// 使能计数器
+	TIM_Cmd(TIM3, ENABLE);
+}
+
+
+void GENERAL_TIM_Init(void)
+{
+
+	TIM3_GPIO_Config();
+	TIM3_NVIC_Config();
+	TIM3_Mode_Config();
+}
+void TIM2_Init()
+{
+    TIM_TimeBaseInitTypeDef  TIM_TimeBaseStructure;
+	NVIC_InitTypeDef NVIC_InitStructure;
+
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM2, ENABLE); //时钟使能
+
+	TIM_TimeBaseStructure.TIM_Period = 9999; //设置在下一个更新事件装入活动的自动重装载寄存器周期的值	 计数到5000为500ms
+	TIM_TimeBaseStructure.TIM_Prescaler =7199; //设置用来作为TIMx时钟频率除数的预分频值  10Khz的计数频率  
+	TIM_TimeBaseStructure.TIM_ClockDivision = 0; //设置时钟分割:TDTS = Tck_tim
+	TIM_TimeBaseStructure.TIM_CounterMode = TIM_CounterMode_Up;  //TIM向上计数模式
+	TIM_TimeBaseInit(TIM2, &TIM_TimeBaseStructure); //根据TIM_TimeBaseInitStruct中指定的参数初始化TIMx的时间基数单位
+ 
+	TIM_ITConfig(  //使能或者失能指定的TIM中断
+		TIM2, //TIM2
+		TIM_IT_Update ,
+		ENABLE  //使能
+		);
+	NVIC_InitStructure.NVIC_IRQChannel = TIM2_IRQn;  //TIM3中断
+	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 2;  //先占优先级0级
+	NVIC_InitStructure.NVIC_IRQChannelSubPriority = 2;  //从优先级3级
+	NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE; //IRQ通道被使能
+	NVIC_Init(&NVIC_InitStructure);  //根据NVIC_InitStruct中指定的参数初始化外设NVIC寄存器
+
+	TIM_Cmd(TIM2, ENABLE);  //使能TIMx外设
+							 
+}
+
+
+/*********************************************END OF FILE**********************/
