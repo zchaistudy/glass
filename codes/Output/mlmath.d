803d6802865af166a09c8adf7ce60ba4244ab11f
study <2468490895@qq.com>
2018-06-05 22:18:14 +0800

    与dev合并

--- codes/Output/mlmath.d
+++
@@ -1,2 +0,0 @@
-..\..\output\mlmath.o: ..\..\User\MPU6050\mllite\mlmath.c
-..\..\output\mlmath.o: G:\software\keil5\ARM\ARMCC\bin\..\include\math.h
--- codes/Output/queue.d
+++
@@ -1,4 +0,0 @@
-..\..\output\queue.o: ..\..\User\queue.c
-..\..\output\queue.o: ..\..\User\queue.h
-..\..\output\queue.o: G:\software\keil5\ARM\ARMCC\bin\..\include\stdio.h
-..\..\output\queue.o: G:\software\keil5\ARM\ARMCC\bin\..\include\stdlib.h
